30d9d3c5dcd31d970b3daf76badb17e8bedda470
akon <45310603+akon1248@users.noreply.github.com>
2021-12-09 00:27:25 +0900

    Remove @Plugin annotation

--- src/main/kotlin/com/example/exampleplugin/ExamplePlugin.kt
+++ src/main/kotlin/com/example/exampleplugin/ExamplePlugin.kt
@@ -1,9 +1,7 @@
 package com.example.exampleplugin
 
-import kr.entree.spigradle.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 
-@Plugin
 class ExamplePlugin : JavaPlugin() {
 
 	override fun onEnable() {
